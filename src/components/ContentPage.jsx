40939a3e49d16e808b56d6901b9f5cf4cb24fab6
k1ns3 <k1ns3za@gmail.com>
2020-07-15 14:45:47 +0300

    [itemCard][refactor] fixed _content.sass files. Fix ContentPage.jsx jsx structure

--- src/components/ContentPage.jsx
+++ src/components/ContentPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Ice, Sun, wheelf32 } from '../assets/img';
+import { Ice, Sun } from '../assets/img';
 import { ProductCard, FilterMenu } from './index';
 
 function ContentPage({ sortItems }) {
@@ -19,14 +19,14 @@ function ContentPage({ sortItems }) {
             <div className="main-top__content">
               <div className="sort">
                 <div className="sort_top">
-                  <span className="sort_top-header">Сортировать: </span>
+                  <span className="sort-header">Сортировать: </span>
                   {sortItems &&
                     sortItems.map((name, index) => (
                       <a
                         className={
                           activeSortItem === index
-                            ? 'sort_top-content__active_top'
-                            : 'sort_top-content'
+                            ? 'sort-content active_top'
+                            : 'sort-content'
                         }
                         key={`${name}_${index}`}
                         onClick={() => toggleActiveSortItem(index)}>
@@ -46,39 +46,23 @@ function ContentPage({ sortItems }) {
               <ProductCard tireType={Ice} />
               <ProductCard tireType={Ice} />
             </div>
+
+            {/*{TODO: переписать этот блок}*/}
             <div className="main-bottom">
               <div className="main-top__content">
                 <div className="sort">
-                  <a className="sort-content__active_bottom" href="#">
-                    1
-                  </a>
-                  <a className="sort-content" href="#">
-                    2
-                  </a>
-                  <a className="sort-content" href="#">
-                    3
-                  </a>
-                  <a className="sort-content" href="#">
-                    4
-                  </a>
-                  <a className="sort-content" href="#">
-                    5
-                  </a>
-                  <a className="main-button main-button__text" href="#">
-                    Далее
-                  </a>
+                  <a className="sort-content active_bottom">1</a>
+                  <a className="sort-content">2</a>
+                  <a className="sort-content">3</a>
+                  <a className="sort-content">4</a>
+                  <a className="sort-content">5</a>
+                  <a className="main-button main-button__text">Далее</a>
                 </div>
                 <div className="limit top">
                   <span className="top-header">Показать</span>
-                  <a className="top-content__active" href="#">
-                    60
-                  </a>
-                  <a className="top-content" href="#">
-                    120
-                  </a>
-                  <a className="top-content" href="#">
-                    240
-                  </a>
+                  <a className="top-content__active">60</a>
+                  <a className="top-content">120</a>
+                  <a className="top-content">240</a>
                 </div>
               </div>
             </div>

0f12dfc234869ea1a69bf45a72f70eaf48b41036
k1ns3 <k1ns3za@gmail.com>
2020-07-24 16:33:00 +0300

    [Redux][fix] to functional component. redux hooks

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,45 +1,38 @@
 import React from 'react';
 import { Aside, ContentPage, Hero } from '../components';
-import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { setTires } from '../redux/actions/tires';
 
 // Главная страница
 
-class Home extends React.Component {
-  componentDidMount() {
-    fetch('http://localhost:3000/tireDataBase.json').then((response) =>
-      response.json().then((data) => this.props.setTires(data.tires))
-    );
-  }
+function Home() {
+  const dispatch = useDispatch();
+  const { items } = useSelector(({ tires }) => {
+    return {
+      items: tires.items,
+    };
+  });
 
-  render() {
-    return (
-      <div>
-        <Hero />
-        <Aside />
-        <ContentPage
-          sortItems={[
-            { name: 'по умолчанию', type: 'popular' },
-            { name: 'возрастанию цены', type: 'priceTop' },
-            { name: 'убыванию цены', type: 'priceDown' },
-          ]}
-          items={this.props.items}
-        />
-      </div>
+  React.useEffect(() => {
+    fetch('http://localhost:3000/tireDataBase.json').then((response) =>
+      response.json().then((data) => dispatch(setTires(data.tires)))
     );
-  }
+  }, []);
+
+  return (
+    <div>
+      <Hero />
+      <Aside />
+      <ContentPage
+        sortItems={[
+          { name: 'по умолчанию', type: 'popular' },
+          { name: 'возрастанию цены', type: 'priceTop' },
+          { name: 'убыванию цены', type: 'priceDown' },
+        ]}
+        items={items}
+      />
+    </div>
+  );
 }
 
-const mapStateToProps = (state) => {
-  return {
-    items: state.tires.items,
-  };
-};
-
-const mapDispathToProps = (dispatch) => {
-  return {
-    setTires: (items) => dispatch(setTires(items)),
-  };
-};
-
-export default connect(mapStateToProps, mapDispathToProps)(Home);
+export default Home;
